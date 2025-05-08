76a2093072c12ad62b9de1820ceba69f1d5c17a3
SayHelloLexa <kds539489@gmail.com>
2025-05-08 14:15:59 +0300

    Upd api

--- API/api.go
+++ API/api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/SayHelloLexa/searcher/pkg/index"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 type API struct {
@@ -33,9 +34,11 @@ func (api *API) endpoints() {
 	api.Router.HandleFunc("/api/v1/docs/{id}", api.getDocById).Methods(http.MethodGet)
 	api.Router.HandleFunc("/api/v1/docs", api.getDocStorage).Methods(http.MethodGet)
 	api.Router.HandleFunc("/api/v1/docs/{id}", api.deleteDocById).Methods(http.MethodDelete)
-	api.Router.HandleFunc("/api/v1/{id}", api.createDoc).Methods(http.MethodPost)
+	api.Router.HandleFunc("/api/v1/docs/{id}", api.createDoc).Methods(http.MethodPost)
+	api.Router.HandleFunc("/api/v1/docs/{id}", api.updDocById).Methods(http.MethodPut, http.MethodPatch)
 }
 
+// getDocStorage - получить все документы
 func (api *API) getDocStorage(w http.ResponseWriter, r *http.Request) {
 	encoder := json.NewEncoder(w)
 	err := encoder.Encode(api.storage)
@@ -44,6 +47,7 @@ func (api *API) getDocStorage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// getDocById - получить документ по id
 func (api *API) getDocById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
@@ -60,9 +64,15 @@ func (api *API) getDocById(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// createDoc - создает документ и добавляет в хранилище
 func (api *API) createDoc(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
+
+	if _, ok := api.storage[id]; ok {
+		http.Error(w, "в хранилище уже присутствует документ с данным id, попробуйте присвоить другое", http.StatusInternalServerError)
+		return
+	}
 
 	decoder := json.NewDecoder(r.Body)
 	var doc crawler.Document
@@ -75,6 +85,32 @@ func (api *API) createDoc(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 }
 
+// updDocById - обновляет документ в хранилище по id
+func (api *API) updDocById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if _, ok := api.storage[id]; !ok {
+		http.Error(w, "документ не найден в хранилище storage", http.StatusInternalServerError)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var doc crawler.Document
+	err := decoder.Decode(&doc)
+	if err != nil {
+		http.Error(w, "ошибка при диссереализации тела запроса", http.StatusInternalServerError)
+	}
+
+	if strconv.Itoa(doc.ID) != id {
+		http.Error(w, "id в теле запроса не совпадает с id в хранилище storage и URL", http.StatusInternalServerError)
+	}
+
+	api.storage[id] = doc
+	w.WriteHeader(http.StatusCreated)
+}
+
+// deleteDocById - удаляет документ из хранилища по id
 func (api *API) deleteDocById(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
